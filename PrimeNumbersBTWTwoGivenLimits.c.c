00842a87d38486cf53353b80ee3d442ce67954e9
Akhila <akhila.vaibhavi@gmail.com>
2026-09-02 18:00:39 +0530

    class

---
+++ PrimeNumbersBTWTwoGivenLimits.c.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int lowerLimit,UpperLimit;
+    printf("Enter any two numbers between which you want to find the prime numbers");
+    scanf("%d,%d",&lowerLimit,&upperLimit);
+
+   printf("Factors of %d are: ",lowerLimit);
+    
+    for (i = 1; i <= num; ++i) {
+        if (num % i == 0) {
+            printf("%d ", i);
+        }
+    }
+  
+
+
+
+
+    return 0;
+}
