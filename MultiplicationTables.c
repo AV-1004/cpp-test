d9bed9f21097ac30f99093c0bd0bc73d33c50ad5
Akhila <akhila.vaibhavi@gmail.com>
2026-09-03 22:20:04 +0530

    multiplicatiotable

--- MultiplicationTables.c
+++ MultiplicationTables.c
@@ -1,13 +1,15 @@
-#include<stdio.h>
-int main(void)
-{
-    int a,i;
-    printf("Enter a number\n");
-    scanf("%d",&a);
-     
-    while(i=i++){
-    for(a=1;a<=10;a=a*i)
-    printf("%d*%d=%d",a,i++,a*i);
+#include <stdio.h>
 
+int main()
+{
+    int i,a;
+    printf("enter a number\n");
+    scanf("%d",&a);
+
+    for (i = 1; i <= 10; i++)
+    {
+        printf("%d * %d = %d\n",a,i,a*i);
     }
+
+    return 0;
 }
