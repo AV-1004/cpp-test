7c2090388bef0b96475fe4108d9e94a0faeae318
Akhila <akhila.vaibhavi@gmail.com>
2026-09-03 21:53:55 +0530

    1to10skippingeven

---
+++ MultiplicationTables.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(void)
+{
+    int a,i;
+    printf("Enter a number\n");
+    scanf("%d",&a);
+     
+    while(i=i++){
+    for(a=1;a<=10;a=a*i)
+    printf("%d*%d=%d",a,i++,a*i);
+
+    }
+}
